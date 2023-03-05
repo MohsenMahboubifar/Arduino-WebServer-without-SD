0f3fae780a902a2bd48a2839205a3ca872448b1e
mohsen <mohsen.mahboubifar@gmail.com>
2023-03-05 15:35:16 +0100

    test first page

--- WebServerWithoutSD.ino
+++ WebServerWithoutSD.ino
@@ -2,26 +2,43 @@
 #include <Ethernet.h>
 #include <SD.h>
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 #define IP IPAddress(192,168,0,129)
+#define req_len 500
 
-EthernetServer server(22336);       // create a server at port 80
-File webFile;                    // handle to files on SD card
-char HTTP_req[40] = {0}; // buffered HTTP request stored as null terminated string
-byte req_index = 0;              // index into HTTP_req buffer
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte req[req_len];
+int req_index = 0;
+
+EthernetServer server(80);       // create a server at port 80
+
+           // index into HTTP_req buffer
 bool root = false;               // login indicator
+
+byte StrContains(char *str, char *sfind)
+{
+    //byte len = strlen(str); 
+    #define LEN strlen(str) 
+    byte found = 0;
+    byte index = 0;
+      
+    if (strlen(sfind) > LEN) return 0;
+    while (index < LEN) 
+    {
+        if (str[index] == sfind[found]) 
+        {
+            found++;
+            if (strlen(sfind) == found) return index;    
+        }
+        else found = 0;
+        index++;
+    }
+    return 0;
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);       // for debugging    
-    // initialize SD card
-    Serial.println("Initializing SD card...");
-    if (!SD.begin(4))         // 4 - SS pin
-    {
-        Serial.println("ERROR - SD card initialization failed!");
-        return;               // init failed
-    }
-    Serial.println("SUCCESS - SD card initialized.");
-    if(!Ethernet.begin(mac))
+    //if(!Ethernet.begin(mac))
     {
       Ethernet.begin(mac, IP);  // initialize Ethernet device => Ethernet.begin(mac, ip);
     }
@@ -38,8 +55,35 @@ void loop() {
     Serial.println("Client is here");
     if(client.available())
     {
-      Serial.println("Server is available!");
-      
+      char ch;
+      while(client.connected())
+      {
+        if(client.available())
+        {
+          ch=client.read();
+          req[req_index++] = ch;
+          Serial.print(ch);
+        }
+        if(ch=='\n')
+        {
+          if(StrContains(req, "GET /"))
+          {
+            Serial.println("Server is available!");
+            Serial.println(millis());
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connnection: close");
+            client.println();
+            client.print("<html><head><title>IP : ");
+            client.print(Ethernet.localIP());
+            client.print("</title></head> <body>b</body></html>");
+            delay(5);
+            client.stop();
+            delay(5);
+          }
+        }
+      }
     }
   }
+delay(5000);
 }
